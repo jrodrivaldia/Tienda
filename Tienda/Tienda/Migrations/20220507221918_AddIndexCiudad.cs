2c4e05ef83351669f37652846f979f184d587c3b
jrvalencia <jrodrivaldia@hotmail.com>
2022-05-07 17:40:04 -0500

    Primera clase Enty

---
+++ Tienda/Tienda/Migrations/20220507221918_AddIndexCiudad.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tienda.Migrations
+{
+    public partial class AddIndexCiudad : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_Name",
+                table: "Countries",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_Name",
+                table: "Countries");
+        }
+    }
+}
