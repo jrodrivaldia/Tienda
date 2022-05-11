ebdf33395db418f9e6c11b3866cf560c3e0a2058
jrvalencia <jrodrivaldia@hotmail.com>
2022-05-10 21:33:15 -0500

    Categories CRUD

--- Tienda/Tienda/Data/DataContext.cs
+++ Tienda/Tienda/Data/DataContext.cs
@@ -3,15 +3,17 @@ using Tienda.Data.Entities;
 
 namespace Tienda.Data
 {
-    public class DataContext: DbContext
+    public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {                
+        {
         }
-        public DbSet <Country> Countries { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
         }
     }
